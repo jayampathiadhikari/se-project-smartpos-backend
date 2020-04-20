710a6a2d0cc7816faf06b0dfbd6d15ea07068772
SachiniAbeygunawardhana <47135264+SachiniAbeygunawardhana@users.noreply.github.com>
2020-04-20 15:03:36 +0530

    Add files via upload

---
+++ src/models/employeeModel.js
@@ -0,0 +1,14 @@
+const { getData } = require('../db/index');
+const { insertData } = require('../db/index');
+
+exports.getUserData = async (req) => {
+  console.log('de')
+    const result = await getData('employee', ['employee_id'], 1345);
+    return result;
+}
+
+exports.insertData = async (req) => {
+  console.log('de')
+    const result =await insertData('employee_role_type', ['role_id','role_name'], [13,'ergrgruurg']);
+    return result;
+}
