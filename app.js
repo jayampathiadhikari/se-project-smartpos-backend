cf600c5dac44680df6326da50172080f9d50004c
SachiniAbeygunawardhana <47135264+SachiniAbeygunawardhana@users.noreply.github.com>
2020-04-23 00:39:04 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import todoRouter from './src/routes/toDoRoutes.js';
+//import express from 'express';
+const express = require('express');
+//import bodyParser from 'body-parser';
+const bodyParser = require('body-parser');
+//import todoRouter from './src/routes/toDoRoutes.js';
+const todoRouter = require('./src/routes/toDoRoutes.js');
+//Access-Control-Allow-Origin: http://localhost:3000;
 
 /**
  *when importing use filename.js otherwise wont work
@@ -15,10 +19,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //router is middleware. to use it follows
 app.get('/', function(req, res) {
+  // const customers=[
+  //   {id:1, firstName:'John', lastName:'Doe'},
+  //   {id:2, firstName:'Jo', lastName:'Do'}
+  // ];
+  // res.json(customers);
     res.send({'name':'jayamoathi'});
 });
 app.use(todoRouter);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
