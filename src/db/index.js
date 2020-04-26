421fedae7dfd0f6974e3d2d682adabe1da8aad3f
Sachini Abeygunawardhana <sachinideepasika.97@gmail.com>
2020-04-26 21:58:20 +0530

    update data

--- src/db/index.js
+++ src/db/index.js
@@ -25,6 +25,26 @@ async function insertData(table_name, column_names, values) {
 
 }
 
+async function updateData(table_name, column_names,values, constraint, constraintvalue) {
+  // UPDATE weather SET (temp_lo, temp_hi, prcp) = (temp_lo+1, temp_lo+15, DEFAULT)
+  //   WHERE city = 'San Francisco' AND date = '2003-07-03';
+  var valuesfinal=''
+  values.forEach((value) => {
+              console.log(value);
+            valuesfinal+="'"+ value+"'"+","
+
+          });
+
+valuesfinal = valuesfinal.slice(0, -1);
+console.log(valuesfinal);
+  const text = `update ${table_name} set (${column_names}) =(${valuesfinal}) where ${constraint}=$1 `
+  //const text = `update ${table_name} set (first_name,street) =('Sam','Muwagama') where ${constraint}=$1`
+
+  const result=await connection.queryParameterized(text,[`${constraintvalue}`]);
+  return result;
+
+}
 
 
-module.exports = { getData,insertData };
+
+module.exports = { getData,insertData,updateData};
