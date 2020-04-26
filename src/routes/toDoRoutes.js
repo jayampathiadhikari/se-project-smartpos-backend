c98416c92c75849e4e891ada967871a6cad5e668
Sachini Abeygunawardhana <sachinideepasika.97@gmail.com>
2020-04-26 21:59:05 +0530

    edit route

--- src/routes/toDoRoutes.js
+++ src/routes/toDoRoutes.js
@@ -15,16 +15,15 @@ router.get('/db', toDoController.databaseTest);
 
 router.get('/employee/profile', employeeController.getUserData);
 router.get('/employee/auth', employeeController.getAuthData);
-router.get('/employee/insert', employeeController.insertData);
+router.post('/employee/insert', employeeController.insertData);
 router.get('/employee/count', employeeController.getCount);
+router.post('/employee/update', employeeController.updateUserData);
+router.post('/employee/edit', employeeController.editUserData);
 
 //router.get('/ownerProfile', ownerController.getUserData);
 
 
 router.get('/api/v1/todos', toDoController.getAllTodos);
 router.get('/api/v1/todos/:id', toDoController.getTodo);
-router.post('/api/v1/todos', toDoController.createTodo);
-router.put('/api/v1/todos/:id', toDoController.updateTodo);
-router.delete('/api/v1/todos/:id', toDoController.deleteTodo);
 
 module.exports= router;
