ae708052a76c5ccd7e87a0156b130e049e4ef967
jayampathi <jayampathiadhikari@gmail.com>
2020-05-08 02:49:44 +0530

    cors headers

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 //router is middleware. to use it follows
 app.get('/', function(req, res) {
     res.send({'name':'jayamoathi'});
--- src/controllers/employeeController.js
+++ src/controllers/employeeController.js
@@ -21,7 +21,7 @@ class Employee {
   }
 
   async addEmployee(req,res){
-    res.setHeader('Access-Control-Allow-Origin','http://localhost:3000')
+    
     //role ids : owner - 3, salesperson - 2 , agent - 1
     const result = await employeeModel.addNewEmployee(req);
     if (result.success) {
