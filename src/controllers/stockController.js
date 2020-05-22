617914768860368af147631237d50f993b026c6a
jayampathi <jayampathiadhikari@gmail.com>
2020-05-22 17:24:17 +0530

    stock controller send with success

--- src/controllers/stockController.js
+++ src/controllers/stockController.js
@@ -81,17 +81,7 @@ class Stock{
   async viewWarehouseStock(req, res) {
 
     const result = await stockModel.getWarehouse(req);
-    if (result.success) {
-      //res.setHeader('Access-Control-Allow-Origin','http://localhost:3000')
-      return res.send(result.data)
-      //console.log(result.data);
-    } else {
-      return res.status(200).send({
-        success: result.success,
-        errorType: result.errorType,
-        error: result.error
-      });
-    }
+    return res.send(result)
   }
 }
 
