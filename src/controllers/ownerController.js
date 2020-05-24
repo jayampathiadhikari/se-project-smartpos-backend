33dd3df6bbbf00214819040430220365998149a4
jayampathi <jayampathiadhikari@gmail.com>
2020-05-25 00:43:16 +0530

    bug fixes in getSuggestionData

--- src/controllers/ownerController.js
+++ src/controllers/ownerController.js
@@ -1,4 +1,3 @@
-
 // import ownerModel from '../models/ownerModel.js';
 
 const ownerModel = require('../models/ownerModel.js');
@@ -22,9 +21,8 @@ class Owner {
   }
 
 
-
   async acceptShopSuggestion(req, res) {
-    const shop_suggestion_id=req.body.shop_suggestion_id;
+    const shop_suggestion_id = req.body.shop_suggestion_id;
     const shopdata = await ownerModel.getSuggestionData(req);
     console.log(shopdata)
     if (shopdata.success) {
@@ -35,40 +33,36 @@ class Owner {
 
       const route_id = shopdata.data[0].route_id
       const district_id = shopdata.data[0].district_id
-      const name= shopdata.data[0].name
-      const latitude= shopdata.data[0].latitude
-      const longitude= shopdata.data[0].longitude
-      const shop_contact_num= shopdata.data[0].shop_contact_num
-      const name_with_initial= shopdata.data[0].name_with_initial
-      const contact_num_cell= shopdata.data[0].contact_num_cell
-      const contact_num_land= shopdata.data[0].contact_num_land
-      const residence_lattitude= shopdata.data[0].residence_lattitude
-      const residence_longitude= shopdata.data[0].residence_longitude
-      const email=shopdata.data[0].email
+      const name = shopdata.data[0].name
+      const latitude = shopdata.data[0].latitude
+      const longitude = shopdata.data[0].longitude
+      const shop_contact_num = shopdata.data[0].shop_contact_num
+      const name_with_initial = shopdata.data[0].name_with_initial
+      const contact_num_cell = shopdata.data[0].contact_num_cell
+      const contact_num_land = shopdata.data[0].contact_num_land
+      const residence_lattitude = shopdata.data[0].residence_lattitude
+      const residence_longitude = shopdata.data[0].residence_longitude
+      const email = shopdata.data[0].email
 
 
-      const insertshopowner= await ownerModel.alterShopShopOWner(shop_suggestion_id,district_id,name_with_initial,contact_num_cell,contact_num_land,residence_lattitude,residence_longitude,email,route_id,name,latitude,longitude,shop_contact_num);
+      const insertshopowner = await ownerModel.alterShopShopOWner(shop_suggestion_id, district_id, name_with_initial, contact_num_cell, contact_num_land, residence_lattitude, residence_longitude, email, route_id, name, latitude, longitude, shop_contact_num);
       //console.log(insertshopowner);
-
-      if (insertshopowner.success){
-
-          res.setHeader('Access-Control-Allow-Origin','*')
-          return res.status(200).send(insertshopowner)
-
-
-    }
-    else{
-        res.setHeader('Access-Control-Allow-Origin','*')
-      return res.status(200).send({
-        success: insertshopowner.success,
-        errorType: insertshopowner.errorType,
-        error: insertshopowner.error
-      });
-    }
-  }
-
-    else {
-      res.setHeader('Access-Control-Allow-Origin','*')
+      if (insertshopowner.success) {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        return res.status(200).send(insertshopowner)
+      } else {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+        return res.status(200).send({
+          success: insertshopowner.success,
+          errorType: insertshopowner.errorType,
+          error: insertshopowner.error
+        });
+      }
+    } else {
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
       return res.status(200).send({
         success: shopdata.success,
         errorType: shopdata.errorType,
@@ -79,7 +73,7 @@ class Owner {
 
 
   async declineShopSuggestion(req, res) {
-    const shop_suggestion_id=req.body.shop_suggestion_id;
+    const shop_suggestion_id = req.body.shop_suggestion_id;
 
     const result = await ownerModel.deleteSuggestion(shop_suggestion_id);
     if (result.success) {
@@ -131,30 +125,25 @@ class Owner {
     }
   }
 
-  async viewMonthlyTarget(req,res){
-      const result = await ownerModel.getTarget(req);
-      //console.log(result);
+  async viewMonthlyTarget(req, res) {
+    const result = await ownerModel.getTarget(req);
+    //console.log(result);
 
-      if (result.success) {
-        //res.setHeader('Access-Control-Allow-Origin','http://localhost:3000')
-        return res.status(200).send(result)
+    if (result.success) {
+      //res.setHeader('Access-Control-Allow-Origin','http://localhost:3000')
+      return res.status(200).send(result)
 
-      } else {
-        return res.status(404).send({
-          success: result.success,
-          errorType: result.errorType,
-          error: result.error
-        });
+    } else {
+      return res.status(404).send({
+        success: result.success,
+        errorType: result.errorType,
+        error: result.error
+      });
     }
   }
 
 
-
-
 }
-
-
-
 
 
 const owner = new Owner();
