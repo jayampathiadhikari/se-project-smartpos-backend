2a7fa19cc86380b65414aa6397ab1b7c9a68cdf1
jayampathi <jayampathiadhikari@gmail.com>
2020-05-29 16:46:17 +0530

    bug fixes

--- src/models/agentModel.js
+++ src/models/agentModel.js
@@ -12,6 +12,6 @@ exports.getSalesDates = async (req) => {
 
 exports.insertSuggestions = async (req) => {
 
-    const result = await insertData('shop_suggestions', ['name','route_id','latitude','longitude','shop_contact_num','name_with_initial','contact_num_cell','contact_num_land','residence_lattitude','residence_longitude','email'], [req.body.name,req.body.route_id,req.body.latitude,req.body.longitude,req.body.shop_contact_num,req.body.name_with_initial,req.body.contact_num_cell,req.body.contact_num_land,req.body.residence_lattitude,req.body.residence_longitude,req.body.email]);
+    const result = await insertData('shop_suggestions', ['name','route_id','latitude','longitude','shop_contact_num','name_with_initial','contact_num_cell','contact_num_land','residence_lattitude','residence_longitude','email','district_id'], [req.body.name,req.body.route_id,req.body.latitude,req.body.longitude,req.body.shop_contact_num,req.body.name_with_initial,req.body.contact_num_cell,req.body.contact_num_land,req.body.residence_lattitude,req.body.residence_longitude,req.body.email,req.body.district_id]);
     return result;
 }
