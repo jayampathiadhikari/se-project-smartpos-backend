857b79b909405a612e4aff2171d85dec4c6434df
jayampathi <jayampathiadhikari@gmail.com>
2020-06-08 01:03:30 +0530

    jsonwebtoken added

---
+++ src/todo.js
@@ -0,0 +1,4 @@
+// todo: employeeModel.addUserAgent() -> addUser() PS:only for agent
+// for this method also send generatedtoken as a para and store it in the db
+// generate the token using emplooyee_id
+
